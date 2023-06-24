62349a4364091f21e98f16ef11cab16270691bab
Beibei Qi <qibeibei1998@gmail.com>
2023-06-24 16:21:38 -0400

    edit address-book component

--- src/app/account/address-book/address-book.component.ts
+++ src/app/account/address-book/address-book.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {UserInfoService} from "../../shared/service/user-info.service";
+import {UserInfo} from "../../shared/model/UserInfo";
+import {ActivatedRoute} from "@angular/router";
+import {switchMap} from "rxjs";
 
 @Component({
   selector: 'app-address-book',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./address-book.component.scss']
 })
 export class AddressBookComponent {
+  userInfo!: UserInfo;
+
+  constructor(public userInfoService: UserInfoService,
+              private route:ActivatedRoute) {
+    userInfoService.userInfo && route.parent?.paramMap.pipe(switchMap(params =>{
+      const id = Number(params.get("id"));
+      return userInfoService.getUserInfo(id);
+    })).subscribe(res =>{
+      if (res.success) {
+        userInfoService.userInfo = res.data;
+      } else {
+        console.log(res);
+      }
+    })
+  }
 
 }
